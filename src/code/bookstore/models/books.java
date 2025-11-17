303ce9b143a814e3b22e0cdfd9a063b19d06bc05
Koi <bealiruao@gmail.com>
2025-11-17 11:45:13 +0700

    added setter functions

--- src/code/bookstore/models/books.java
+++ src/code/bookstore/models/books.java
@@ -1,5 +1,49 @@
 package code.bookstore.models;
 
 public class books {
-    
+    private String bookId;
+    private String bookName;
+    private String authorId;
+    private double price;
+    private String publisherId;
+
+    public books() {}
+
+    public books(String bookId, String bookName, String authorId, double price, String publisherId){
+        this.bookId = bookId;
+        this.bookName = bookName;
+        this.authorId = authorId;
+        this.price = price;
+        this.publisherId = publisherId;
+    }
+
+    public void setBookId(String bookId){
+        this.bookId = bookId;
+    }
+
+    public void setbookName(String bookName){
+        this.bookName = bookName;
+    }
+
+    public void setauthorId(String authorId){
+        this.authorId = authorId;
+    }
+
+    public void setprice(double price){
+        this.price = price;
+    }
+
+    public void setpublisherId(String publisherId){
+        this.publisherId = publisherId;
+    }
+
+    public String getInfo(){
+        return "Book{" +
+                "bookId:" + bookId + '\'' +
+                ", bookName:" + bookName + '\'' +
+                ", authorId:" + authorId + '\'' +
+                ", publisherId:" + publisherId + '\'' +
+                ", price:" + price + 
+                "}";
+    }
 }
