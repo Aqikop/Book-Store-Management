4847e8290fa8656302641c74a18f2286c24ef363
Koi <bealiruao@gmail.com>
2025-11-27 10:12:16 +0700

    feat: checkout view

---
+++ src/code/bookstore/views/checkoutview.java
@@ -0,0 +1,198 @@
+package code.bookstore.views;
+
+import javax.swing.*;
+import java.awt.image.*;
+import java.io.File;
+import javax.imageio.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class checkoutview {
+    public JPanel init_panel(){
+        JPanel checkout = new JPanel(new BorderLayout());
+        checkout.setBackground(Color.WHITE);
+
+        JPanel header = new JPanel(new BorderLayout());
+        header.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createMatteBorder(0, 0, 1, 0, Color.decode("#e0e0e0")),
+                BorderFactory.createEmptyBorder(15, 15, 15, 15)
+            ));
+        JLabel header_text = new JLabel("Review order");
+        header_text.setBorder(BorderFactory.createEmptyBorder(6, 12, 6, 12));
+        header_text.setFont(new Font("Lato", Font.BOLD, 18));
+        header.add(header_text, BorderLayout.WEST);
+
+        
+        JPanel review = new JPanel();
+        review.setLayout(new BoxLayout(review, BoxLayout.Y_AXIS));
+        review.setBackground(Color.WHITE);
+        review.setBorder(BorderFactory.createCompoundBorder(
+            BorderFactory.createLineBorder(Color.decode("#e0e0e0"), 3),
+            BorderFactory.createEmptyBorder(20, 20, 20, 20)
+        ));
+        review.setPreferredSize(new Dimension(500, 0));
+
+        // Sample data 
+        String[] titles = {"The Heart-Shaped Tin", "To Kill a Mockingbird"};
+        String[] authors = {"Bee Wilson", "Harper Lee"};
+        String[] publishers = {"Penguin Books", "Harper Perennial"};
+        double[] prices = {19.99, 15.99};
+
+        // Iterate through search result (i < n)
+        for(int i = 0; i < 2; i++){
+            JPanel book_entry = new JPanel(new BorderLayout());
+            book_entry.setBackground(Color.decode("#ffffff"));
+            book_entry.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createMatteBorder(0, 0, 1, 0, Color.decode("#e0e0e0")),
+                BorderFactory.createEmptyBorder(15, 15, 15, 15)
+            ));
+            book_entry.setMaximumSize(new Dimension(Integer.MAX_VALUE, 170));
+            book_entry.setPreferredSize(new Dimension(0, 170));
+
+            JLabel book_cover_label = new JLabel();
+            try{
+                BufferedImage book_cover = ImageIO.read(new File("src\\resources\\images\\"));
+                Image scaled = book_cover.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+                book_cover_label.setIcon(new ImageIcon(scaled));
+            } catch(Exception e){
+                book_cover_label.setText("No img available");
+                book_cover_label.setPreferredSize(new Dimension(150, 110));
+                book_cover_label.setHorizontalAlignment(JLabel.CENTER);
+                book_cover_label.setVerticalAlignment(JLabel.CENTER);
+                book_cover_label.setOpaque(false);
+            }
+            book_cover_label.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 20));
+
+            // Book info panel
+            JPanel book_info = new JPanel();
+            book_info.setLayout(new BoxLayout(book_info, BoxLayout.Y_AXIS));
+            book_info.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+            book_info.setOpaque(false);
+
+            JLabel book_title = new JLabel(titles[i]);
+            book_title.setFont(new Font("Lato", Font.PLAIN, 25));
+            book_title.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+            JLabel book_author = new JLabel("by " + authors[i]);
+            book_author.setFont(new Font("Lato", Font.ITALIC, 14));
+            book_author.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+            JLabel book_price = new JLabel("$" + String.format("%.2f", prices[i]));
+            book_price.setFont(new Font("Lato", Font.BOLD, 25));
+            book_price.setForeground(Color.decode("#26ed5b"));
+            book_price.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+            // Quantity selection
+            JPanel quantity_choose = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+            quantity_choose.setAlignmentX(Component.LEFT_ALIGNMENT);
+            quantity_choose.setOpaque(false);
+
+            JLabel quantity_text = new JLabel("Quantity:");
+            quantity_text.setFont(new Font("Lato", Font.PLAIN, 14));
+
+            JSpinner quantity_scoller = new JSpinner(new SpinnerNumberModel(1, 1, 99 ,1));
+            quantity_scoller.setFont(new Font("Lato", Font.PLAIN, 14));
+            quantity_scoller.setPreferredSize(new Dimension(60, 30));
+
+            JLabel update_price = new JLabel("Update");
+            update_price.setFont(new Font("Lato", Font.PLAIN, 14));
+
+            quantity_choose.add(quantity_text);
+            quantity_choose.add(Box.createRigidArea(new Dimension(2, 0)));
+            quantity_choose.add(quantity_scoller);
+            quantity_choose.add(Box.createRigidArea(new Dimension(1, 0)));
+            quantity_choose.add(update_price);
+
+            book_info.add(book_title);
+            book_info.add(book_author);
+            book_info.add(book_price);
+            book_info.add(quantity_choose);
+
+            book_entry.add(book_cover_label, BorderLayout.WEST);
+            book_entry.add(book_info, BorderLayout.CENTER);
+
+
+            review.add(book_entry);
+
+            if(i < 1){
+                review.add(Box.createRigidArea(new Dimension(0, 10)));
+            }
+        }
+       
+        
+        JPanel order_summary = new JPanel(new BorderLayout());
+        order_summary.setPreferredSize(new Dimension(280, 0));
+        order_summary.setBackground(Color.WHITE);
+        order_summary.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createMatteBorder(0, 0, 1, 0, Color.decode("#e0e0e0")),
+                BorderFactory.createEmptyBorder(15, 15, 15, 15)
+            ));
+        JLabel summary_text = new JLabel("Order Summary");
+        summary_text.setFont(new Font("Lato", Font.BOLD, 18));
+        summary_text.setForeground(Color.decode("#2c3e50"));
+        summary_text.setBorder(BorderFactory.createEmptyBorder(0 ,0, 10, 0));
+        order_summary.add(summary_text, BorderLayout.NORTH);
+        
+        JPanel summary_content = new JPanel();
+        summary_content.setLayout(new BoxLayout(summary_content, BoxLayout.Y_AXIS));
+        summary_content.setOpaque(false);
+
+        summary_content.add(price_row("Item (2):", "$100"));
+        summary_content.add(price_row("Shipping", "$9.99"));
+
+        JSeparator separator = new JSeparator();
+        separator.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+        separator.setMaximumSize(new Dimension(Integer.MAX_VALUE, 1));
+        summary_content.add(separator);
+
+        JPanel total_row = new JPanel(new BorderLayout());
+        total_row.setOpaque(false);
+        total_row.setMaximumSize(new Dimension(Integer.MAX_VALUE, 35));
+        total_row.setBorder(BorderFactory.createEmptyBorder(5, 0, 15, 0));
+
+        JLabel total = new JLabel("Total:");
+        total.setFont(new Font("Lato", Font.PLAIN, 15));
+        total.setForeground(Color.BLACK);
+
+        JLabel total_price = new JLabel("$1000");
+        total_price.setFont(new Font("Lato", Font.BOLD, 15));
+        total_price.setForeground(Color.BLACK);
+
+        total_row.add(total, BorderLayout.WEST);
+        total_row.add(total_price, BorderLayout.EAST);
+        summary_content.add(total_row);
+
+        order_summary.add(summary_content, BorderLayout.CENTER);
+
+        
+
+        // JPanel ship_info = new JPanel();
+
+        checkout.add(header, BorderLayout.NORTH);
+        checkout.add(review, BorderLayout.CENTER);
+        checkout.add(order_summary, BorderLayout.EAST);
+        // checkout.add(ship_info, BorderLayout.SOUTH);
+
+        return checkout;
+    }
+
+    private JPanel price_row(String row, String price){
+        JPanel price_row = new JPanel(new BorderLayout());
+        price_row.setMaximumSize(new Dimension(Integer.MAX_VALUE, 25));
+        price_row.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
+        price_row.setOpaque(false);
+
+        JLabel row_name = new JLabel(row);
+        row_name.setFont(new Font("Lato", Font.PLAIN, 13));
+        row_name.setForeground(Color.BLACK);
+
+        JLabel row_price = new JLabel(price);
+        row_price.setFont(new Font("Lato", Font.PLAIN, 13));
+        row_price.setForeground(Color.BLACK);
+
+        price_row.add(row_name, BorderLayout.WEST);
+        price_row.add(row_price, BorderLayout.EAST);
+
+        return price_row;
+    }
+}
