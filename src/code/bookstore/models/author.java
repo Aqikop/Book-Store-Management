870c56c8821cc61f8fb4987e9d7f8ae2330cef62
Koi <bealiruao@gmail.com>
2025-11-29 23:52:45 +0700

    feat: get name by id function

---
+++ src/code/bookstore/models/author.java
@@ -0,0 +1,86 @@
+package code.bookstore.models;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import code.bookstore.utils.DbConnect;
+
+public class author extends extra{
+    private String authorId;
+    private String authorName;
+    
+    public author(){
+        this.authorId = null;
+        this.authorName = null;
+    }
+
+    public author(String authorId, String authorName){
+        this.authorId = authorId;
+        this.authorName = authorName;
+    }
+
+    @Override
+    public String generateId(){
+        DbConnect newconn = new DbConnect();
+        Connection conn = newconn.connect();
+
+        String new_id = null;
+        String query = "SELECT 'A' || LPAD(nextval('auth_id_seq')::text, 4, '0') AS auth_id";
+
+        try {
+            PreparedStatement stm = conn.prepareStatement(query);
+            ResultSet rs = stm.executeQuery();
+
+            if(rs.next()){
+                new_id = rs.getString("author_id");
+            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return new_id;
+    }
+
+    public void setId(String authorId){
+        this.authorId = authorId;
+    }
+
+    public void setauthorName(String authorName){
+        this.authorName = authorName;
+    }
+
+    public String getInfo(){
+        return "Author{" +
+                "authorId:" + authorId + '\'' +
+                ", authorName:" + authorName + 
+                "}";
+    }
+
+    public static String get_id_by_name(String authorName){
+        DbConnect newconn = new DbConnect();
+        Connection conn = newconn.connect();
+
+        String query = "SELECT author_id FROM author WHERE LOWER(author_name) = LOWER(?)";
+        try(PreparedStatement stm = conn.prepareStatement(query)){
+            stm.setString(1, authorName);
+            ResultSet rs = stm.executeQuery();
+            
+            if(rs.next()){
+                String authorId = rs.getString("author_id");
+                conn.close();
+                return authorId;
+            }
+            else{ 
+                conn.close();
+                author new_author = new author(); 
+                return new_author.generateId();
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
